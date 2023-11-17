5c6cffef7f08c7796665172f17bda0e4b7a7910d
Narayan sharma <narayan2k01@gmail.com>
2023-11-17 19:37:07 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,102 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [postalCode, setPostalCode] = useState('');
+  const [locationInfo, setLocationInfo] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState('');
+
+  const handleInputChange = (event) => {
+    setPostalCode(event.target.value);
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    // Check if postalCode is empty
+    if (!postalCode) {
+      setError('Please enter a postal code.');
+      return;
+    }
+
+    setLoading(true);
+
+    try {
+      const response = await fetch(`https://api.zippopotam.us/in/${postalCode}`);
+
+      if (!response.ok) {
+        throw new Error('Invalid postal code. Please enter a valid one.');
+      }
+
+      const data = await response.json();
+      setLocationInfo(data);
+      setError('');
+    } catch (err) {
+      setLocationInfo(null);
+      setError(err.message || 'Error fetching location information. Please try again.');
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const handleClear = () => {
+    setPostalCode('');
+    setLocationInfo(null);
+    setError('');
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Postal Code App</h1>
+
+      <form onSubmit={handleSubmit} className="form-container">
+        <label>
+          Enter Postal Code:
+          <input
+            type="text"
+            maxLength="10"
+            minLength="6"
+            placeholder="Postal code"
+            value={postalCode}
+            onChange={handleInputChange}
+          />
+        </label>
+        <button type="submit" disabled={loading}>
+          {loading ? 'Fetching Information' : '⬇️ Search'}
+        </button>
+        {locationInfo && (
+          <button onClick={handleClear} className="clear-button">
+            Clear Information
+          </button>
+        )}
+      </form>
+
+      {error && <p className="error">{error}</p>}
+
+      {locationInfo && (
+        <div className="location-info-container">
+          <h2>Location Information:</h2>
+          <p>Country: {locationInfo.country}</p>
+          {locationInfo.places && locationInfo.places.length > 0 && (
+            <div>
+              <p>State: {locationInfo.places[0].state}</p>
+              <p>Place Name: {locationInfo.places[0]['place name']}</p>
+              <div className="mapbox">
+                <div className="map-container">
+                  <iframe title="Google Map" src={`https://maps.google.com/maps?q=${encodeURIComponent (locationInfo.places[0]['place name'])}&t=&z=13&ie=UTF8&iwloc=&output=embed`}
+                    width="300" 
+                    height="150" 
+                    allowFullScreen 
+                  ></iframe>
+                </div>
+              </div>
+            </div>
+          )}
+        </div>
+      )}
     </div>
   );
-}
+};
 
 export default App;
